cbd4f380b6101510816cf22c141456179e86c1ac
kamel Barakat <kamelbarakat96@yahoo.com>
2022-07-27 19:25:25 -0400

    Changes Made

--- index.js
+++ index.js
@@ -1,2 +1,29 @@
 
 // Select some elements...
+
+//aligning text left
+let header = document.querySelector('#page-header')
+header.style.textAlign = "left"
+header.style.backgroundColor = "blue"
+
+
+//changing dog image
+
+let dogImages = document.querySelectorAll('.dog-image')
+for(let i = 0; i < dogImages.length; i++){
+dogImages[i].style.borderRadius = '50px'
+dogImages[i].style.rotate = '180deg'
+}
+
+//changing dog text align left
+let dogName = document.querySelectorAll(".dog-name")
+for(let i = 0; i < dogName.length; i++){
+    dogName[i].style.textAlign = 'left'
+    }
+
+
+
+//footer style
+footer.style.borderStyle = 'solid'
+footer.style.color = "pink"
+footer.style.backgroundColor = "orange"
